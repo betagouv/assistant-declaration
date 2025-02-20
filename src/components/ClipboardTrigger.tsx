b4c0b13b245bcad2431da05caf36988305e4d680
Thomas Ramé <thomas.rame@outlook.com>
2025-02-20 15:55:29 +0100

    fuuuuu

--- src/components/ClipboardTrigger.tsx
+++ src/components/ClipboardTrigger.tsx
@@ -9,8 +9,15 @@ export interface ClipboardTriggerProps {
 export function ClipboardTrigger({ onCopy, children }: PropsWithChildren<ClipboardTriggerProps>) {
   const wrapperRef = useRef<HTMLTableElement | null>(null);
 
+  // We have to use an unicity check because in development React is using the strict mode
+  // and renders the component twice. Due to this we also cannot use `useState(() => false)` because the 2 renders
+  // are totally distincts. Having 2 copy actions at the same time is breaking having HTML part into the clipboard (whereas the text part stays)
+  const copiedRef = useRef(false);
+
   useEffect(() => {
-    if (wrapperRef.current) {
+    if (!copiedRef.current && wrapperRef.current) {
+      copiedRef.current = true;
+
       htmlToClipboard(wrapperRef.current.innerHTML).then(() => {
         onCopy && onCopy();
       });
