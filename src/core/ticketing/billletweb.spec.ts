d5fa693d15efcd5707bd8890f4ee27e03484745b
Thomas Ramé <thomas.rame@outlook.com>
2025-02-24 14:35:48 +0100

    chore(ticketing): split files as more ticketing systems will be implemented

---
+++ src/core/ticketing/billletweb.spec.ts
@@ -0,0 +1,25 @@
+import { secondsToMilliseconds, set } from 'date-fns';
+
+import { BilletwebTicketingSystemClient } from '@ad/src/core/ticketing/billetweb';
+
+const describeWhenManual = process.env.TEST_MANUAL === 'true' ? describe : describe.skip;
+const itWhenManual = process.env.TEST_MANUAL === 'true' ? it : it.skip;
+
+describeWhenManual('BilletwebTicketingSystemClient', () => {
+  const client = new BilletwebTicketingSystemClient(process.env.TEST_BILLETWEB_ACCESS_KEY || '', process.env.TEST_BILLETWEB_SECRET_KEY || '');
+
+  describe('getEventsSeries()', () => {
+    it(
+      'should return events in specific timeframe',
+      async () => {
+        const fromDate = set(new Date(0), { year: 2024, month: 11, date: 18 });
+        const toDate = set(new Date(0), { year: 2024, month: 12, date: 1 });
+
+        const wrappers = await client.getEventsSeries(fromDate, toDate);
+
+        expect(wrappers.length).toBeGreaterThanOrEqual(10);
+      },
+      secondsToMilliseconds(30)
+    );
+  });
+});
