dc028e66d3ec1e0ff13cebadde2f96150e5001ca
Thomas Ramé <thomas.rame@outlook.com>
2025-03-11 10:26:55 +0100

    chore: adjust home naming

--- src/app/(public)/PublicLayout.tsx
+++ src/app/(public)/PublicLayout.tsx
@@ -22,7 +22,7 @@ export function PublicLayout(props: PropsWithChildren) {
       linkProps: {
         href: linkRegistry.get('home', undefined),
       },
-      text: 'Présentation',
+      text: 'À propos',
     },
     {
       iconId: 'fr-icon-questionnaire-line',
