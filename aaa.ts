af7c29afbe724922b5ae96e01ab9671a3060e889
Thomas Ramé <thomas.rame@outlook.com>
2025-03-26 18:27:09 +0100

    wip

---
+++ aaa.ts
@@ -0,0 +1,23 @@
+import { createClient } from '@hey-api/openapi-ts';
+
+createClient({
+  input: 'https://docapi.weezevent.com/weezticket.json',
+  output: 'src/client/weezevent',
+  plugins: ['@hey-api/client-fetch'],
+});
+
+//
+//
+//
+//
+//
+//
+//
+// https://github.com/hey-api/openapi-ts/issues/1874
+// https://github.com/hey-api/openapi-ts/issues/1874
+// https://github.com/hey-api/openapi-ts/issues/1874
+// https://github.com/hey-api/openapi-ts/issues/1874
+// https://github.com/hey-api/openapi-ts/issues/1874
+//
+//
+//
