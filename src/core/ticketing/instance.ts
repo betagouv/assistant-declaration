4178cfc5e5c5824ebc5a43c666bb0a21cb0b03cd
Thomas Ramé <thomas.rame@outlook.com>
2025-05-06 20:41:09 +0200

    feat(ticketing): implement soticket system [DB REQUIRED !!!]

--- src/core/ticketing/instance.ts
+++ src/core/ticketing/instance.ts
@@ -3,6 +3,7 @@ import { TicketingSystem } from '@prisma/client';
 import { BilletwebTicketingSystemClient } from '@ad/src/core/ticketing/billetweb';
 import { MockTicketingSystemClient, TicketingSystemClient } from '@ad/src/core/ticketing/common';
 import { MapadoTicketingSystemClient } from '@ad/src/core/ticketing/mapado';
+import { ShotgunTicketingSystemClient } from '@ad/src/core/ticketing/shotgun';
 import { SoticketTicketingSystemClient } from '@ad/src/core/ticketing/soticket';
 import { SupersoniksTicketingSystemClient } from '@ad/src/core/ticketing/supersoniks';
 import { workaroundAssert as assert } from '@ad/src/utils/assert';
@@ -30,6 +31,12 @@ export function getTicketingSystemClient(
         assert(ticketingSystem.apiSecretKey);
 
         ticketingSystemClient = new MapadoTicketingSystemClient(ticketingSystem.apiSecretKey);
+        break;
+      case 'SHOTGUN':
+        assert(ticketingSystem.apiAccessKey);
+        assert(ticketingSystem.apiSecretKey);
+
+        ticketingSystemClient = new ShotgunTicketingSystemClient(ticketingSystem.apiAccessKey, ticketingSystem.apiSecretKey);
         break;
       case 'SOTICKET':
         assert(ticketingSystem.apiAccessKey);
