f1140ee2c193ef15b24a63d1213436e7e42fc22c
Thomas Ramé <thomas.rame@outlook.com>
2025-05-20 16:47:47 +0200

    aaa

--- src/app/(public)/(home)/Partners.tsx
+++ src/app/(public)/(home)/Partners.tsx
@@ -1,13 +1,8 @@
 import { fr } from '@codegouvfr/react-dsfr';
-import { Box, Container, Grid, GridProps, Link, Typography } from '@mui/material';
-import Image from 'next/image';
-import NextLink from 'next/link';
+import { Box, Container, GridProps, Link, Typography } from '@mui/material';
 import { ImgHTMLAttributes } from 'react';
 
-import billetweb from '@ad/src/assets/images/partners/billetweb.png';
-import mapado from '@ad/src/assets/images/partners/billetweb.png';
-import supersoniks from '@ad/src/assets/images/partners/billetweb.png';
-import soticket from '@ad/src/assets/images/partners/billetweb.png';
+import { useLiveChat } from '@ad/src/components/live-chat/useLiveChat';
 
 export const gridItemProps: GridProps = {
   xs: 6,
@@ -29,78 +24,37 @@ export const imageProps: ImgHTMLAttributes<HTMLImageElement> = {
 };
 
 export function Partners() {
+  const { showLiveChat, isLiveChatLoading } = useLiveChat();
+
   return (
     <Container sx={{ pt: 1, pb: { xs: 4, sm: 5, md: 6 } }}>
-      <Typography component="h2" variant="h4" sx={{ textAlign: 'center', mt: 4, mb: { xs: 3, sm: 4 } }}>
-        Les éditeurs de billetterie compatibles
-      </Typography>
-      <Box sx={{ minHeight: { xs: 236, sm: 144, md: 52 } }}>
-        <Grid container spacing={4} justifyContent="center">
-          <Grid item {...gridItemProps}>
-            <Link
-              component={NextLink}
-              href="https://www.billetweb.fr"
-              target="_blank"
-              underline="none"
-              sx={{
-                backgroundImage: 'none !important',
-                '&::after': {
-                  display: 'none !important',
-                },
-              }}
-            >
-              <Image src={billetweb} alt="logo de Billetweb" style={{ ...imageProps.style }} />
-            </Link>
-          </Grid>
-          <Grid item {...gridItemProps}>
-            <Link
-              component={NextLink}
-              href="https://www.mapado.com"
-              target="_blank"
-              underline="none"
-              sx={{
-                backgroundImage: 'none !important',
-                '&::after': {
-                  display: 'none !important',
-                },
-              }}
-            >
-              <Image src={mapado} alt="logo de Mapado" style={{ ...imageProps.style }} />
-            </Link>
-          </Grid>
-          <Grid item {...gridItemProps}>
-            <Link
-              component={NextLink}
-              href="https://supersoniks.com"
-              target="_blank"
-              underline="none"
-              sx={{
-                backgroundImage: 'none !important',
-                '&::after': {
-                  display: 'none !important',
-                },
-              }}
-            >
-              <Image src={supersoniks} alt="logo de Supersoniks" style={{ ...imageProps.style }} />
-            </Link>
-          </Grid>
-          <Grid item {...gridItemProps}>
-            <Link
-              component={NextLink}
-              href="https://www.socoop.fr"
-              target="_blank"
-              underline="none"
-              sx={{
-                backgroundImage: 'none !important',
-                '&::after': {
-                  display: 'none !important',
-                },
-              }}
-            >
-              <Image src={soticket} alt="logo de SoTicket" style={{ ...imageProps.style }} />
-            </Link>
-          </Grid>
-        </Grid>
+      <Box
+        sx={{
+          bgcolor: fr.colors.decisions.background.actionLow.blueFrance.default,
+          textAlign: 'center',
+          maxWidth: 800,
+          mx: 'auto',
+          borderRadius: '5px',
+          px: 3,
+          py: 6,
+        }}
+      >
+        <Typography variant="h6">Vous êtes éditeur ?</Typography>
+        <Typography variant="body1">
+          <Link
+            component="button"
+            onClick={showLiveChat}
+            underline="none"
+            sx={{
+              '&::after': {
+                display: 'none !important',
+              },
+            }}
+          >
+            Contactez notre équipe technique
+          </Link>{' '}
+          pour proposer le service à vos clients !
+        </Typography>
       </Box>
     </Container>
   );
