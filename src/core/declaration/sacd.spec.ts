94872d69a615125bd463fec8cbd5bd5175cf654d
Thomas Ramé <thomas.rame@outlook.com>
2025-06-03 15:32:38 +0200

    aaa

--- src/core/declaration/sacd.spec.ts
+++ src/core/declaration/sacd.spec.ts
@@ -4,6 +4,8 @@
 import { secondsToMilliseconds, set } from 'date-fns';
 
 import { SacdClient } from '@ad/src/core/declaration/sacd';
+import { sacdDeclarations } from '@ad/src/fixtures/declaration/sacd';
+import { eventsSeries, eventsWrappers } from '@ad/src/fixtures/event';
 
 const describeWhenManual = process.env.TEST_MANUAL === 'true' ? describe : describe.skip;
 const itWhenManual = process.env.TEST_MANUAL === 'true' ? it : it.skip;
@@ -17,12 +19,28 @@ describeWhenManual('SacdClient', () => {
     process.env.TEST_SACD_API_PROVIDER_PASSWORD || ''
   );
 
+  beforeAll(async () => {
+    await client.login();
+  }, secondsToMilliseconds(10));
+
+  afterAll(async () => {
+    if (client.loggedIn()) {
+      await client.logout();
+    }
+  }, secondsToMilliseconds(10));
+
   it(
     'should test authenticated flow',
     async () => {
-      await client.login();
       await client.test();
-      await client.logout();
+    },
+    secondsToMilliseconds(30)
+  );
+
+  it(
+    'should test a declaration submission',
+    async () => {
+      await client.declare('TODO', eventsSeries[0], eventsWrappers, sacdDeclarations[0]);
     },
     secondsToMilliseconds(30)
   );
--- src/core/declaration/sacd.ts
+++ src/core/declaration/sacd.ts
@@ -1,10 +1,16 @@
 import crypto from 'crypto';
 import { format } from 'date-fns';
+import { default as libphonenumber } from 'google-libphonenumber';
 
 import { getExcludingTaxesAmountFromIncludingTaxesAmount, getTaxAmountFromIncludingTaxesAmount } from '@ad/src/core/declaration';
+import { useServerTranslation } from '@ad/src/i18n';
+import { SacdAccountingCategorySchema, SacdDeclarationSchemaType, SacdProductionTypeSchema } from '@ad/src/models/entities/declaration/sacd';
 import { EventSerieSchemaType, EventWrapperSchemaType } from '@ad/src/models/entities/event';
 import { JsonHelloWorldResponseSchema, JsonLoginResponseSchema } from '@ad/src/models/entities/sacd';
 import { workaroundAssert as assert } from '@ad/src/utils/assert';
+import { convertInputModelToGooglePhoneNumber } from '@ad/src/utils/phone';
+
+const phoneNumberUtil = libphonenumber.PhoneNumberUtil.getInstance();
 
 export class SacdClient {
   protected readonly baseUrl: string = process.env.SACD_API_BASE_URL || 'https://nowhere';
@@ -36,6 +42,10 @@ export class SacdClient {
 
   protected getAccessToken(): string {
     return crypto.createHash('md5').update(`${this.secretKey}${this.authToken}`).digest('hex');
+  }
+
+  public loggedIn(): boolean {
+    return this.authToken !== null;
   }
 
   public async login() {
@@ -109,6 +119,10 @@ export class SacdClient {
     const responseObject = JsonHelloWorldResponseSchema.parse(responseJson);
   }
 
+  protected formatString(value: string) {
+    return value.slice(0, 128); // 128 chars max according to their documentation
+  }
+
   protected formatDate(date: Date) {
     return format(date, 'yyyy/MM/dd');
   }
@@ -117,26 +131,35 @@ export class SacdClient {
     return format(date, 'HH:mm');
   }
 
-  public async declare(declarantId: string, eventSerie: EventSerieSchemaType, wrappers: EventWrapperSchemaType[]) {
+  public async declare(
+    declarantId: string,
+    eventSerie: EventSerieSchemaType,
+    wrappers: EventWrapperSchemaType[],
+    declaration: SacdDeclarationSchemaType
+  ) {
     assert(this.authToken);
     assert(wrappers.length > 0);
+
+    const { t } = useServerTranslation('common');
 
     const queryParams = new URLSearchParams({
       key: this.consumerKey,
       token: this.getAccessToken(),
     });
 
-    // TODO:
-    // TODO:
-    // TODO: keep in memory success calls to not retry those if there are an error
-    // TODO:
-    // TODO:
+    const bodyParams = new URLSearchParams(this.commonBodyParams);
+    bodyParams.append('parameters[dec_ref]', declarantId);
+    bodyParams.append('parameters[dec_systeme]', 'ASSISTANT_DECLARATION');
+    bodyParams.append('parameters[dec_date]', this.formatDate(new Date()));
+    bodyParams.append('parameters[dec_nb]', wrappers.length.toString());
 
-    // TODO: truncate strings 128chars
-
+    // Order events for the ease when debugging
     const sortedWrappers = wrappers.sort((a, b) => a.event.startAt.getTime() - b.event.startAt.getTime());
 
-    // Since we cannot declare all events of an event serie at once, we have loop over each event and declare it one by one
+    // After contacting the SACD support they said amounts scoped to the event serie should be filled only once
+    // on the first event of the serie (maybe they have case of people splitting them per event but we don't)
+    let eventSerieSpecificsFilled = false;
+
     for (const wrapper of sortedWrappers) {
       let totalIncludingTaxesAmount: number = 0;
       let paidTickets: number = 0;
@@ -155,16 +178,11 @@ export class SacdClient {
         totalIncludingTaxesAmount += total * price;
       }
 
-      const bodyParams = new URLSearchParams(this.commonBodyParams);
-      bodyParams.append('parameters[dec_ref]', declarantId);
-      bodyParams.append('parameters[dec_systeme]', 'ASSISTANT_DECLARATION');
-      bodyParams.append('parameters[dec_date]', this.formatDate(new Date()));
-      bodyParams.append('parameters[dec_nb]', '1');
       bodyParams.append('parameters[rep_ref_dec]', wrapper.event.id);
-      bodyParams.append('parameters[rep_devise]', 'EUR');
+      bodyParams.append('parameters[rep_devise]', 'EUR'); // When synchronizing events series we refuse other currency so it's safe to be hardcoded for now
       bodyParams.append('parameters[rep_statut]', 'DEF');
       bodyParams.append('parameters[rep_version]', '1');
-      bodyParams.append('parameters[rep_titre]', eventSerie.name);
+      bodyParams.append('parameters[rep_titre]', this.formatString(eventSerie.name));
       bodyParams.append('parameters[rep_date_debut]', this.formatDate(wrapper.event.startAt));
       bodyParams.append('parameters[rep_date_fin]', this.formatDate(wrapper.event.endAt));
       bodyParams.append('parameters[rep_nb]', '1');
@@ -182,31 +200,115 @@ export class SacdClient {
       bodyParams.append('parameters[rep_nb_billets_pay]', paidTickets.toString());
       bodyParams.append('parameters[rep_nb_billets_exo]', freeTickets.toString());
 
-      bodyParams.append('parameters[rep_mt_cession]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_tx_tva_cession]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_mt_tva_cession]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_mt_frais]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_mt_apports_coprod]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_mt_garantie_rec]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_mt_depenses]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_mt_autres]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[expl_nat]', true ? 'AMA' : 'PRO');
-      bodyParams.append('parameters[expl_type_ref]', 'TODOOOOOOOO');
+      if (!eventSerieSpecificsFilled) {
+        eventSerieSpecificsFilled = true;
+
+        const saleOfRights = declaration.accountingEntries.find(
+          (accountingEntry) => accountingEntry.category === SacdAccountingCategorySchema.Values.SALE_OF_RIGHTS
+        );
+        const saleOfRightsTaxRate = saleOfRights?.taxRate ?? 0;
+
+        bodyParams.append(
+          'parameters[rep_mt_cession]',
+          saleOfRights ? getExcludingTaxesAmountFromIncludingTaxesAmount(saleOfRights.includingTaxesAmount, saleOfRightsTaxRate).toString() : '0'
+        );
+        bodyParams.append('parameters[rep_tx_tva_cession]', (100 * saleOfRightsTaxRate).toString()); // Providing 5.5% as 5.5 instead of 0.055
+        bodyParams.append(
+          'parameters[rep_mt_tva_cession]',
+          saleOfRights ? getTaxAmountFromIncludingTaxesAmount(saleOfRights.includingTaxesAmount, saleOfRightsTaxRate).toString() : '0'
+        );
+
+        const introductionFees = declaration.accountingEntries.find(
+          (accountingEntry) => accountingEntry.category === SacdAccountingCategorySchema.Values.INTRODUCTION_FEES
+        );
+        const introductionFeesTaxRate = introductionFees?.taxRate ?? 0;
+
+        bodyParams.append(
+          'parameters[rep_mt_frais]',
+          introductionFees
+            ? getExcludingTaxesAmountFromIncludingTaxesAmount(introductionFees.includingTaxesAmount, introductionFeesTaxRate).toString()
+            : '0'
+        );
+
+        const coproductionContribution = declaration.accountingEntries.find(
+          (accountingEntry) => accountingEntry.category === SacdAccountingCategorySchema.Values.COPRODUCTION_CONTRIBUTION
+        );
+        const coproductionContributionTaxRate = coproductionContribution?.taxRate ?? 0;
+
+        bodyParams.append(
+          'parameters[rep_mt_apports_coprod]',
+          coproductionContribution
+            ? getExcludingTaxesAmountFromIncludingTaxesAmount(
+                coproductionContribution.includingTaxesAmount,
+                coproductionContributionTaxRate
+              ).toString()
+            : '0'
+        );
+
+        const revenueGuarantee = declaration.accountingEntries.find(
+          (accountingEntry) => accountingEntry.category === SacdAccountingCategorySchema.Values.REVENUE_GUARANTEE
+        );
+        const revenueGuaranteeTaxRate = revenueGuarantee?.taxRate ?? 0;
+
+        bodyParams.append(
+          'parameters[rep_mt_garantie_rec]',
+          revenueGuarantee
+            ? getExcludingTaxesAmountFromIncludingTaxesAmount(revenueGuarantee.includingTaxesAmount, revenueGuaranteeTaxRate).toString()
+            : '0'
+        );
+
+        // // The budget is not something we are collecting from declarants
+        // bodyParams.append(
+        //   'parameters[rep_mt_depenses]',
+        //   'TODO'
+        // );
+
+        const other = declaration.accountingEntries.find(
+          (accountingEntry) => accountingEntry.category === SacdAccountingCategorySchema.Values.INTRODUCTION_FEES
+        );
+        const otherTaxRate = other?.taxRate ?? 0;
+
+        bodyParams.append(
+          'parameters[rep_mt_autres]',
+          other ? getExcludingTaxesAmountFromIncludingTaxesAmount(other.includingTaxesAmount, otherTaxRate).toString() : '0'
+        );
+      }
+
+      bodyParams.append('parameters[expl_nat]', declaration.productionType === SacdProductionTypeSchema.Values.AMATEUR ? 'AMA' : 'PRO');
+
+      // bodyParams.append('parameters[expl_type_ref]', 'TODOOOOOOOO');
       // bodyParams.append('parameters[expl_ref]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[rep_public]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[salle_jauge]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[salle_prix_moyen]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[diff_nom]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[diff_type_ref]', 'TODOOOOOOOO');
 
-      // TODO: set diffuseur or not?... since they have it
+      // TODO: SACD confirmed it must be per event whereas we did the audience global to the event serie, it should be changed
+      // Note: they have not defined values so giving them french translations we have
+      const repPublicTranslation = t(`model.sacdDeclaration.audience.enum.${declaration.audience}`);
 
-      bodyParams.append('parameters[salle_nom]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[diff_ville]', 'TODOOOOOOOO');
-      bodyParams.append('parameters[diff_pays]', 'TODOOOOOOOO');
+      bodyParams.append('parameters[rep_public]', this.formatString(repPublicTranslation));
+
+      bodyParams.append('parameters[salle_nom]', this.formatString(declaration.placeName));
+      bodyParams.append('parameters[salle_jauge]', declaration.placeCapacity.toString());
+
+      // TODO: should this be for the event or for the event serie?
+      bodyParams.append('parameters[salle_prix_moyen]', declaration.averageTicketPrice.toString());
+
+      bodyParams.append('parameters[diff_nom]', this.formatString(declaration.organizer.name));
+      // bodyParams.append('parameters[diff_type_ref]', 'TODOOOOOOOO');
+      // bodyParams.append('parameters[diff_ref]', 'TODOOOOOOOO');
+      bodyParams.append('parameters[diff_adresse_1]', this.formatString(declaration.organizer.headquartersAddress.street));
+      bodyParams.append('parameters[diff_code_postal]', this.formatString(declaration.organizer.headquartersAddress.postalCode));
+      bodyParams.append('parameters[diff_ville]', this.formatString(declaration.organizer.headquartersAddress.city));
+      bodyParams.append('parameters[diff_pays]', this.formatString(declaration.organizer.headquartersAddress.countryCode)); // Not translating the country for now
+      bodyParams.append(
+        'parameters[diff_tel]',
+        this.formatString(
+          phoneNumberUtil.format(convertInputModelToGooglePhoneNumber(declaration.organizer.phone), libphonenumber.PhoneNumberFormat.NATIONAL)
+        )
+      );
+      bodyParams.append('parameters[diff_mel]', this.formatString(declaration.organizer.email));
+      bodyParams.append('parameters[diff_tva]', this.formatString(declaration.organizer.europeanVatId));
 
       // should tva be 0.055 or 5.5 ?
-
+      // should globlal info be on each rep? seems like duplicates
       // clarifier "représentations du groupe" c'est le groupe de déclarations donc ? ...
 
       // TODO:
