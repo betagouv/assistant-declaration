74ada6fcb7f97f15092777c1a5d1f5704f110778
Thomas Ramé <thomas.rame@outlook.com>
2025-06-11 23:09:50 +0200

    aaa

--- src/core/ticketing/dice.ts
+++ src/core/ticketing/dice.ts
@@ -1,8 +1,21 @@
+import { GraphQLClient } from 'graphql-request';
+
+import { getSdk } from '@ad/src/client/dice/generated/graphql';
 import { TicketingSystemClient } from '@ad/src/core/ticketing/common';
 import { LiteEventSerieWrapperSchemaType } from '@ad/src/models/entities/event';
 
 export class DiceTicketingSystemClient implements TicketingSystemClient {
-  constructor(secretKey: string) {}
+  protected graphqlSdk: ReturnType<typeof getSdk>;
+
+  constructor(secretKey: string) {
+    const graphqlClient = new GraphQLClient('https://partners-endpoint.dice.fm/graphql', {
+      headers: {
+        Authorization: `Bearer ${secretKey}`,
+      },
+    });
+
+    this.graphqlSdk = getSdk(graphqlClient);
+  }
 
   public async testConnection(): Promise<boolean> {
     try {
@@ -15,6 +28,8 @@ export class DiceTicketingSystemClient implements TicketingSystemClient {
   }
 
   public async getEventsSeries(fromDate: Date, toDate?: Date): Promise<LiteEventSerieWrapperSchemaType[]> {
+    const events = await this.graphqlSdk.GetEvents();
+
     // TODO:
     return [];
   }
