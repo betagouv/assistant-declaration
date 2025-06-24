7ae9479f0513f93884b85f7caa84ae5f567ce70c
Thomas Ramé <thomas.rame@outlook.com>
2025-06-24 14:46:22 +0200

    test patching json schema

--- src/client/helloasso.config.ts
+++ src/client/helloasso.config.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@hey-api/openapi-ts';
 
 createClient({
-  input: 'https://raw.githubusercontent.com/HelloAsso/helloasso-open-api/refs/heads/main/helloasso.json',
+  // input: 'https://raw.githubusercontent.com/HelloAsso/helloasso-open-api/refs/heads/main/helloasso.json',
+  input: 'src/client/helloasso.json',
   output: 'src/client/helloasso',
   plugins: ['@hey-api/client-fetch'],
 });
