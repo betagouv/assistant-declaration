3775664e5dab064f997a23bf20dab0a77d99c097
Thomas Ramé <thomas.rame@outlook.com>
2025-09-11 19:41:50 +0200

    chore: SignUpPage

--- src/app/(visitor-only)/auth/sign-up/SignUpPage.tsx
+++ src/app/(visitor-only)/auth/sign-up/SignUpPage.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { fr } from '@codegouvfr/react-dsfr';
+import { Alert } from '@codegouvfr/react-dsfr/Alert';
 import humanCooperation from '@gouvfr/dsfr/dist/artwork/pictograms/environment/human-cooperation.svg';
-import { Alert, Grid, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useContext, useState } from 'react';
 
+import styles from '@ad/src/app/(visitor-only)/auth/sign-up/SignUpPage.module.scss';
 import { SignUpPageContext } from '@ad/src/app/(visitor-only)/auth/sign-up/SignUpPageContext';
-import { formTitleProps } from '@ad/src/utils/form';
-import { centeredFormContainerGridProps } from '@ad/src/utils/grid';
+import { formTitleClasses } from '@ad/src/utils/form';
 
 export function SignUpPage() {
   const { ContextualSignUpForm } = useContext(SignUpPageContext);
@@ -16,63 +16,62 @@ export function SignUpPage() {
   const [success, setSuccess] = useState(false);
 
   return (
-    <Grid container>
-      <Grid item xs={12} lg={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Grid container {...centeredFormContainerGridProps}>
-          {success ? (
-            <Alert severity="success">
-              Votre inscription a bien été prise en compte. Vous allez recevoir un email à l&apos;adresse indiquée pour confirmer votre inscription.
-            </Alert>
-          ) : (
-            <>
-              <Typography component="h1" {...formTitleProps}>
-                Inscription
-              </Typography>
-              <ContextualSignUpForm
-                onSuccess={() => {
-                  setSuccess(true);
-                }}
-              />
-            </>
-          )}
-        </Grid>
-      </Grid>
-      <Grid
-        item
-        xs={12}
-        lg={6}
-        container
-        direction={'column'}
-        sx={{
-          background: fr.colors.decisions.background.alt.blueFrance.default,
-          px: 6,
-          py: 4,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Image
-          src={humanCooperation}
-          alt=""
-          priority={true}
+    <div className={fr.cx('fr-container-lg--fluid')} style={{ display: 'flex', flex: 1 }}>
+      <div className={fr.cx('fr-grid-row')} style={{ flex: 1 }}>
+        <div
+          className={fr.cx('fr-col-12', 'fr-col-lg-5', 'fr-px-6v', 'fr-py-8v')}
           style={{
-            backgroundColor: '#f5f5fe', // [WORKAROUND] Simple hack since DSFR does not provide pictograms in dark mode
-            width: '90%',
-            height: 'auto',
-            maxHeight: 250,
-            objectFit: 'contain',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
-        />
-        <Typography component="div" variant="body1" sx={{ pt: '40px' }}>
-          <Typography sx={{ fontWeight: 'bold' }}>Les raisons de s&apos;inscrire sur la plateforme :</Typography>
-          <ol>
-            <li>Un lien direct entre vos données de billetterie et vos déclarations</li>
-            <li>Centraliser gratuitement vos étapes de déclarations</li>
-            <li>Ne plus garder de données spectateurs sensibles sur votre ordinateur</li>
-            <li>Bénéficier d&apos;un outil qui évolue au fil de vos retours utilisateurs</li>
-          </ol>
-        </Typography>
-      </Grid>
-    </Grid>
+        >
+          <div className={fr.cx('fr-grid-row', 'fr-grid-row--gutters')}>
+            <div
+              className={`${fr.cx('fr-col-12', 'fr-col-sm-10', 'fr-col-md-6', 'fr-col-lg-10', 'fr-col-xl-12', 'fr-m-auto')} ${styles.formContainer}`}
+            >
+              {success ? (
+                <Alert
+                  severity="success"
+                  small={false}
+                  title="Succès"
+                  description="Votre inscription a bien été prise en compte. Vous allez recevoir un email à l'adresse indiquée pour confirmer votre inscription."
+                />
+              ) : (
+                <>
+                  <h1 className={fr.cx(...formTitleClasses)}>Inscription</h1>
+                  <ContextualSignUpForm
+                    onSuccess={() => {
+                      setSuccess(true);
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+        <div
+          className={fr.cx('fr-col-12', 'fr-col-lg-7', 'fr-px-12v', 'fr-py-8v')}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: fr.colors.decisions.background.alt.blueFrance.default,
+          }}
+        >
+          <Image src={humanCooperation} alt="" priority={true} className={styles.descriptionImage} />
+          <div className={styles.description}>
+            <span className={fr.cx('fr-text--bold')}>Les raisons de s&apos;inscrire sur la plateforme :</span>
+            <ol>
+              <li>Un lien direct entre vos données de billetterie et vos déclarations</li>
+              <li>Centraliser gratuitement vos étapes de déclarations</li>
+              <li>Ne plus garder de données spectateurs sensibles sur votre ordinateur</li>
+              <li>Bénéficier d&apos;un outil qui évolue au fil de vos retours utilisateurs</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
